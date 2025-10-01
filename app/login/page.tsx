52781375bbf531f7f24cd1b23fbe7278ade887c0
winsondong <winson@bu.edu>
2025-10-01 18:35:36 -0400

    refactor/convert-login-to-mui-wd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,44 +1,74 @@
-// File: app/login.page.tsx
+'use client';
 
-import styles from './page.module.css';
+import * as React from 'react';
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Button,
+  Stack,
+  Link,
+  FormControlLabel,
+  Checkbox,
+} from '@mui/material';
 
 export default function Login() {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    // TODO: form handling
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <h1 className={styles.title}>Login</h1>
+    <Box minHeight="100vh" display="flex" alignItems="center" justifyContent="center" px={2}>
+      <Card sx={{ width: '100%', maxWidth: 400, borderRadius: 3 }} elevation={4}>
+        <CardContent sx={{ p: 4 }}>
+          <Typography variant="h5" align="center" fontWeight={700} color="text.primary" mb={3}>
+            Login
+          </Typography>
 
-        <form className={styles.form}>
-          {/* Email */}
-          <div className={styles.formGroup}>
-            <label htmlFor="email">Email</label>
-            <input id="email" type="email" placeholder="Enter your email" />
-          </div>
+          <Box component="form" onSubmit={handleSubmit}>
+            <Stack spacing={2}>
+              {/* Email */}
+              <TextField id="email" name="email" type="email" label="Email" placeholder="Enter your email" fullWidth />
 
-          {/* Password */}
-          <div className={styles.formGroup}>
-            <label htmlFor="password">Password</label>
-            <input id="password" type="password" placeholder="Enter your password" />
-          </div>
+              {/* Password */}
+              <TextField
+                id="password"
+                name="password"
+                type="password"
+                label="Password"
+                placeholder="Enter your password"
+                fullWidth
+              />
 
-          {/* Remember Me + Forgot Password */}
-          <div className={styles.options}>
-            <label>
-              <input type="checkbox" /> Remember me
-            </label>
-            <a href="#">Forgot password?</a>
-          </div>
+              {/* Remember + Forgot */}
+              <Stack direction="row" alignItems="center" justifyContent="space-between">
+                <FormControlLabel
+                  control={<Checkbox size="small" />}
+                  label={<Typography variant="body2">Remember me</Typography>}
+                />
+                <Link href="#" underline="hover" variant="body2" color="primary">
+                  Forgot password?
+                </Link>
+              </Stack>
 
-          {/* Submit */}
-          <button type="submit" className={styles.button}>
-            Sign In
-          </button>
-        </form>
+              {/* Submit */}
+              <Button type="submit" variant="contained" size="medium" fullWidth>
+                Sign In
+              </Button>
+            </Stack>
+          </Box>
 
-        <p className={styles.footer}>
-          Don&apos;t have an account? <a href="#">Sign up</a>
-        </p>
-      </div>
-    </div>
+          <Typography variant="body2" align="center" mt={3} color="text.secondary">
+            Don&apos;t have an account?{' '}
+            <Link href="#" underline="hover" color="primary">
+              Sign up
+            </Link>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
